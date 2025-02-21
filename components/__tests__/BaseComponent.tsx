934203b5af292d3a2884a2744a6932d607bc1e62
Tran Duc Thinh <ducthinh1341993@gmail.com>
2025-02-21 15:24:09 +0700

    add component screenView vs keyboard scroll

---
+++ components/__tests__/BaseComponent.tsx
@@ -0,0 +1,16 @@
+import { forwardRef, useImperativeHandle } from "react";
+import { View, StyleSheet } from "react-native";
+
+type ExampleProps = {};
+type ExampleRef = {};
+
+const Example = forwardRef(
+  ({}: ExampleProps, ref: React.ForwardedRef<ExampleRef>) => {
+    // Expose to parent component via ref
+    useImperativeHandle(ref, () => ({}));
+
+    return <View></View>;
+  }
+);
+
+const styles = StyleSheet.create({});
---
+++ hooks/useGradualAnimation.ts
@@ -0,0 +1,17 @@
+import { useKeyboardHandler } from "react-native-keyboard-controller";
+import { useSharedValue } from "react-native-reanimated";
+
+export const useGradualAnimation = () => {
+  const height = useSharedValue(0);
+
+  useKeyboardHandler(
+    {
+      onMove: (event) => {
+        "worklet";
+        height.value = Math.max(event.height, 0);
+      },
+    },
+    []
+  );
+  return { height };
+};
