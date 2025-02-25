3f10c7169070caba13d5a61cd123d5824bf1c924
Tran Duc Thinh <ducthinh1341993@gmail.com>
2025-02-25 17:16:56 +0700

    update devdepenci

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,21 @@
 module.exports = function (api) {
-  api.cache(true);
+  api.cache(true)
   return {
-    presets: ['babel-preset-expo'],
-    plugins: [],
-  };
-};
+    presets: ["babel-preset-expo", "module:metro-react-native-babel-preset"],
+    env: {
+      production: {},
+    },
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"], // Thư mục gốc của dự án
+          alias: {
+            "@": "./", // Alias `@` trỏ tới thư mục `src`
+          },
+          extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+        },
+      ],
+    ],
+  }
+}
