543e73187b919cbd85ceb77e55a836581ec3ab38
Tran Duc Thinh <ducthinh1341993@gmail.com>
2025-03-03 16:38:21 +0700

    test UI

--- app/tabs/index.tsx
+++ app/tabs/index.tsx
@@ -1,27 +1,53 @@
-import { Image, StyleSheet, Platform, ScrollView } from 'react-native';
+import { Image, StyleSheet, Platform, ScrollView } from "react-native"
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { CarouselMovie } from '@/components/screen/home/CarouselMovie';
-import { Colors } from '@/constants/Colors';
-import { ScreenView } from '@/components';
-import { HeaderHome } from '@/components/screen/home';
+import { HelloWave } from "@/components/HelloWave"
+import ParallaxScrollView from "@/components/ParallaxScrollView"
+import { ThemedText } from "@/components/ThemedText"
+import { ThemedView } from "@/components/ThemedView"
+import { CarouselMovie } from "@/components/screen/home/CarouselMovie"
+import { Colors } from "@/constants/Colors"
+import { ScreenView } from "@/components"
+import { HeaderHome } from "@/components/screen/home"
+import { MovieList } from "@/components/screen/home/MovieList"
+import { ChatHomeScreen } from "./chatHome"
 
 export default function HomeScreen() {
+  const data = [
+    {
+      id: "1",
+      poster: "https://image.tmdb.org/t/p/original/rdN2CW2SuTEPZ7i6RqpZUpRQQ5X.jpg",
+    },
+    {
+      id: "2",
+      poster: "https://image.tmdb.org/t/p/original/rdN2CW2SuTEPZ7i6RqpZUpRQQ5X.jpg",
+    },
+    {
+      id: "3",
+      poster: "https://image.tmdb.org/t/p/original/rdN2CW2SuTEPZ7i6RqpZUpRQQ5X.jpg",
+    },
+    {
+      id: "5",
+      poster: "https://image.tmdb.org/t/p/original/rdN2CW2SuTEPZ7i6RqpZUpRQQ5X.jpg",
+    },
+    {
+      id: "6",
+      poster: "https://image.tmdb.org/t/p/original/rdN2CW2SuTEPZ7i6RqpZUpRQQ5X.jpg",
+    },
+  ] as any
   return (
-    <ScreenView>
-      <HeaderHome />
-      <CarouselMovie />
+    <ScreenView preset="scroll">
+      <ChatHomeScreen />
+      {/* <HeaderHome /> */}
+      {/* <CarouselMovie /> */}
+      {/* <MovieList movies={data} title="BTV" onViewMore={() => {}} /> */}
     </ScreenView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
@@ -33,6 +59,6 @@ const styles = StyleSheet.create({
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
   },
-});
+})
